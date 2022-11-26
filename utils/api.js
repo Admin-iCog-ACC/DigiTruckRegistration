113e5b63f6849a19498f2e6c08b59f7b4e4f46e8
Admin-iCog-ACC <99467741+Admin-iCog-ACC@users.noreply.github.com>
2022-11-26 16:14:36 +0300

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const register = (data) =>
-  axios.post('http://localhost:3000/api/register', data).then((res) => res.data);
+  axios.post('http://143.198.190.167:5001/api/register', data).then((res) => res.data);
 
 export const acc = (data) =>
-  axios.post('http://localhost:3001/api/acc', data).then((res) => res.data);
+  axios.post('http://143.198.190.167:5001/api/acc', data).then((res) => res.data);
