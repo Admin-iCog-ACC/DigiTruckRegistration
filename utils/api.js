6a569f660a4b5a043b1c4bc637e0613a55b9dea8
AlAswaad99 <dannyboy9917@gmail.com>
2022-11-30 13:59:24 +0300

    fix cors issue - frontend

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const register = (data) =>
-  axios.post('http://localhost:3000/api/register', data).then((res) => res.data);
+  axios.post('https://digitruckethiopia.icogacc.com/api/register', data).then((res) => res.data);
 
 export const acc = (data) =>
-  axios.post('http://localhost:3000/api/acc', data).then((res) => res.data);
+  axios.post('https://digitruckethiopia.icogacc.com/api/acc', data).then((res) => res.data);
