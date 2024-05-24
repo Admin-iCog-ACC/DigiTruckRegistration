4478f274c439c5b5d8558c20f3b249b3ed7e1966
samsonkassa-dev <samsonkassa25@gmail.com>
2024-05-24 16:10:00 +0300

    footer email link

--- components/Footer.js
+++ components/Footer.js
@@ -88,7 +88,7 @@ function Footer() {
                 </span>
                 <span className="flex space-x-1">
                   <p className="font-medium">Email: </p>
-                  <p>info@icog-acc.com</p>
+                  <p>info@icogacc.com</p>
                 </span>
               </div>
               <div>
