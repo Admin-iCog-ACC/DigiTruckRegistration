e0cae3a7b31f09839d540ef23d62c1d25fb702f7
bruk-lemma <bruklemma2017@gmail.com>
2024-09-20 09:48:06 +0300

    added icog logo in partner section and adjust footer email

--- components/Footer.js
+++ components/Footer.js
@@ -95,7 +95,7 @@ function Footer() {
                   </span>
                   <span className="flex space-x-1">
                     <p className="font-medium">Email: </p>
-                    <p>info@icogacc.com</p>
+                    <p>info@icog.et</p>
                   </span>
                 </div>
                 <div>
